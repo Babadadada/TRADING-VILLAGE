e6b7a4bada5794ff9e9ecebeda36840056ff361e
Faisal ALMOTAR <106304319+Babadadada@users.noreply.github.com>
2022-05-29 10:23:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+print("This is your guide to help you achive your dream item")
+name = str(input("What is your name(please do not put a silly name) :"))
+print("Welcome", name, "to the trading village")
+print("you have a botttle of water, there is 2 people who need it this is their trades")
+print("1) a stone 2) a circuit board that is broken")
+#the good answer will be the circuit board because it can contain gold
